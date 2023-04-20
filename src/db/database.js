24267bca56d6690cdbc8834662e68a8f5d12b7f8
Leograzioli <leograzioli@gmail.com>
2023-04-20 21:54:42 +0200

    add modal

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,8 @@
 import campoMinato from '../assets/campo-minato.jpg'
-import avadaBurger from '../assets/avada-burger.jpg'
 import rickMorty from '../assets/rick-morty.jpg'
+import bdBack from '../assets/bd-back.jpg'
+import bdFront from '../assets/bd-front.jpg'
+// import avadaBurger from '../assets/avada-burger.jpg'
 
 export const projects = [
    {
@@ -11,20 +13,36 @@ export const projects = [
       "github" : 'https://github.com/Leograzioli/campominato-2.0',
       "imgPath" : campoMinato
    },
-   {
-      "title": 'Avada Burger Landing Page',
-      "description": 'landing page from a burger place with dynamic content.',
-      "technologies":  ['Html', 'Css', 'Sass', "Javascript", 'Vue.js'],
-      "href" : 'https://avada-burger.netlify.app/',
-      "github" : 'https://github.com/Leograzioli/proj-html-vuejs',
-      "imgPath" : avadaBurger
-   },
+   // {
+   //    "title": 'Avada Burger Landing Page',
+   //    "description": 'landing page from a burger place with dynamic content.',
+   //    "technologies":  ['Html', 'Css', 'Sass', "Javascript", 'Vue.js'],
+   //    "href" : 'https://avada-burger.netlify.app/',
+   //    "github" : 'https://github.com/Leograzioli/proj-html-vuejs',
+   //    "imgPath" : avadaBurger
+   // },
    {
       "title": 'Rick and Morty API',
       "description": 'A simple layout that prints all characters from rick and morty.',
-      "technologies":  ['Html', 'Css', 'Bootstrap', "Javascript", 'Vue.js'],
+      "technologies":  ['Html', 'Css', 'Bootstrap', 'Vue.js'],
       "href" : 'https://lg-rick-morty.netlify.app',
       "github" : 'https://github.com/Leograzioli/rick-marty-api',
       "imgPath" : rickMorty
+   },
+   {
+      "title": 'B-doctors Back-office',
+      "description": 'Back-Office (dashboard) of a profile from a medical social media. made in a team of 5',
+      "technologies":  ['Php', 'Laravel', 'Css ', ' Sass', 'Bootstrap'],
+      "href" : false,
+      "github" : 'https://github.com/Anatoliegit88/B-Doctors',
+      "imgPath" : bdBack
+   },
+   {
+      "title": 'B-doctors Front-office',
+      "description": 'A social media to find Doctors or resgister yourself as a Doctor. made in a team of 5',
+      "technologies":  ['Html', 'Css', 'Sass', 'Bootstrap', 'vue.js'],
+      "href" : false,
+      "github" : 'https://github.com/FRA2721/b-doctors-vite',
+      "imgPath" : bdFront
    },
 ]
--- src/pages/AppProjects.jsx
+++ src/pages/AppProjects.jsx
@@ -1,30 +1,46 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { projects } from '../db/database'
 import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
+import { AiOutlineClose } from 'react-icons/ai'
 
 export default function () {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [img, setImg] = useState('')
+
 
     useEffect(() => {
         document.title = 'Leo Grazioli - Projects';
     }, []);
 
+    const handleClick = (img) => {
+        setIsOpen(true)
+        setImg(img)
+        console.log(projects.length)
+    }
+
     return (
         <>
             <section id='projects' className='max-w-[1200px] mx-auto mt-12'>
                 <h2 className='text-4xl text-center sm:text-start text-transparent bg-clip-text bg-gradient-to-t from-primary-dark to-secondary dark:bg-gradient-to-b dark:from-white dark:to-secondary font-bold pb-2'>My Projects</h2>
+                
+                <div className='mt-16 text-center dark:text-white text-sm relative'> 
+                    <p>*click on a image to preview*</p>
+                    <p className='absolute right-0 top-0'>Projects Found: {projects.length}</p>
+                </div>
 
-                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mt-2 '>
                     {projects.map((project, i) => {
                         return (
-                            <div key={i} className='relative bg-primary-dark text-white dark:text-primary-dark dark:bg-primary max-w-[330px] ms_shadow rounded-tl-xl rounded-br-xl mt-10 mx-auto'>
+                            <div key={i} className='relative bg-primary-dark text-white dark:text-primary-dark dark:bg-primary max-w-[330px] ms_shadow rounded-tl-xl rounded-br-xl mb-8 mx-auto'>
 
                                 {/* icons in absolute position */}
-                                {project.href && <a href={project.href} target='_blank' className='absolute right-4 border top-4 p-2 rounded-full bg-primary-dark dark:text-white '> <BsArrowUpRight /></a>}
+                                {project.href && <a href={project.href} target='_blank' className='absolute right-4 border- top-4 p-2 rounded-full bg-primary-dark dark:text-white '> <BsArrowUpRight /></a>}
                                 {project.github && <a href={project.github} target='_blank' className='absolute border right-4 top-14 text-3xl rounded-full bg-white text-primary-dark'> <BsGithub /></a>}
 
                                 {/* image */}
-                                <div className='rounded-tl-xl rounded-br-xl p-2 overflow-hidden'>
-                                    <img className='rounded-tl-xl rounded-br-xl min-h-[200px] object-cover' src={`${project.imgPath}`} alt="product" />
+                                <div onClick={() => handleClick(project.imgPath)} className='rounded-tl-xl rounded-br-xl p-2 overflow-hidden'>
+                                    <img className='rounded-tl-xl rounded-br-xl h-[200px] w-[314px] object-cover ' src={`${project.imgPath}`} alt="product" />
                                 </div>
 
                                 {/* description */}
@@ -48,6 +64,15 @@ export default function () {
                     }
                     )}
                 </div>
+
+                {/* modal */}
+                {isOpen && <div className='h-screen bg-black/[0.5] absolute top-0 right-0 left-0 z-50 flex justify-center items-center'>
+                    <div className='border-4 border-white relative' >
+                        <p onClick={() =>{ setImg(''), setIsOpen(false) } } className='absolute top-3 right-5 text-2xl bg-secondary rounded-xl text-semibold p-2'><AiOutlineClose /></p>
+                        <img className='max-h-[700px] max-w-[1200px]' src={img} alt="" />
+                    </div>
+                </div>}
+
             </section>
         </>
     )
